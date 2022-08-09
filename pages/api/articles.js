500089725fd9530dc7fe9afcc327bcf74f5a15ad
Jesus <chumchum239@gmail.com>
2022-08-09 02:15:04 -0400

    added cors to api

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -1,6 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import data from "../../data/articles_desc.json";
 
-export default function handler(req, res) {
-  res.status(200).send(data);
+// export default function handler(req, res) {
+//   res.status(200).send(data);
+// }
+
+import NextCors from "nextjs-cors";
+
+export default async function handler(req, res) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  // Rest of the API logic
+  res.send(data);
 }
