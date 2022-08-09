3b9e245e13a5dddbef7ebc2af5d726eb91026c57
Jesus Yanez <96055925+jesusyanez@users.noreply.github.com>
2022-08-09 05:47:39 -0400

    Update articles.js

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -7,7 +7,7 @@ import data from "../../data/articles_desc.json";
 
 import NextCors from "nextjs-cors";
 
-async function handler(req, res) {
+export default async function handler(req, res) {
   // Run the cors middleware
   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
   await NextCors(req, res, {
