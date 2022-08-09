a077939a4623d7bbc7c25d804c7de5f16a86e1a5
Jesus Yanez <96055925+jesusyanez@users.noreply.github.com>
2022-08-09 05:45:38 -0400

    Update articles.js

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -7,13 +7,13 @@ import data from "../../data/articles_desc.json";
 
 import NextCors from "nextjs-cors";
 
-export default async function handler(req, res) {
+async function handler(req, res) {
   // Run the cors middleware
   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
   await NextCors(req, res, {
     // Options
     methods: ["GET"],
-    origin: "*",
+    origin: "https://techline-nextjs.vercel.app/",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
