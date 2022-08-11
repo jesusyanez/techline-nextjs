8765da661c322a022005f1eed5e5588b0bbaab84
Jesus <chumchum239@gmail.com>
2022-08-11 18:55:59 -0400

    aligned images

--- components/drumkit.js
+++ components/drumkit.js
@@ -6,7 +6,7 @@ const Drumkits = ({ drumkit }) => {
   return (
     <a href={drumkit.url}>
       <div className="card w-full bg-base-100 shadow-xl mb-3 cursor-pointer transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-102 hover:bg-accent duration-300">
-        <div className="flex justify-center pt-3">
+        <div className="flex px-6 justify-start h-16 pt-3">
           <img className="rounded-xl" src={drumkit.image} />
         </div>
         <div className="card-body p-6">
