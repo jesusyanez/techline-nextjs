c35ee5f53952429266b048e7ce0d8900212d9fd8
Jesus Yanez <96055925+jesusyanez@users.noreply.github.com>
2023-01-19 21:35:09 -0500

    opened cors

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   await NextCors(req, res, {
     // Options
     methods: ["GET"],
-    origin: "https://techline-nextjs.vercel.app/",
+    origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
